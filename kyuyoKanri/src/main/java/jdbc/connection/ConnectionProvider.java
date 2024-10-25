4be69bf04bfbb3562c5a1b4edcfae22755c3dd17
현서 <@hyunseo>
2024-10-25 11:37:54 +0900

    .

--- kyuyoKanri/src/main/java/jdbc/connection/ConnectionProvider.java
+++ kyuyoKanri/src/main/java/jdbc/connection/ConnectionProvider.java
@@ -4,6 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//public class ConnectionProvider {
+//	public static Connection getConnection() throws SQLException {
+//		// 만들어둔 데이터베이스 커넥션 풀 board와 연결 후 Connection을 반환
+//		return DriverManager.getConnection("jdbc:apache:commons:dbcp:payzone");
+//	}
+//}
 public class ConnectionProvider {
     public static Connection getConnection() throws SQLException {
         // DBCP 없이 직접 JDBC 연결을 생성
